23f0c09ae63e37ebf738a943440ef2d971abef74
Vidied <davidepan2001@gmail.com>
2026-06-08 16:16:36 +0200

    esercizio 1

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -8,6 +9,30 @@ public class Main{
 
         for (int i = 0; i < numeri.length; i++) {
             numeri[i] = (int) (Math.random() * 10) + 1;
+            System.out.println("Numero array creato: " + numeri[i]);
+        }
+
+        while (true) {
+            try {
+                System.out.println("Inserisci il numero da voler aggiungere nell'array di numeri");
+                int numero = scanner.nextInt();
+
+                System.out.println("Inserisci la posizione in cui vuoi mettere il numero 1-5 inserisci 0 se vuoi uscire dal programma");
+                int posizione = scanner.nextInt();
+
+                if (posizione == 0) {
+                    System.out.println("Chiusura del programma");
+                    break;
+                }
+
+                numeri[posizione - 1] = numero;
+
+                System.out.println(Arrays.toString(numeri));
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Errore: posizione non valida");
+            } catch (Exception e) {
+                System.out.println("Errore: errore generico");
+            }
+            }
         }
     }
-}

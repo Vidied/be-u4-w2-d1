e0a09279ead3bfe5d13574926eaf849bcdb78fa9
Vidied <davidepan2001@gmail.com>
2026-06-08 16:46:28 +0200

    esercizio due svolto in maniera migliore fixando dei problemi quando viene scritto una lettera al posto di un numero

--- src/esercizio2/Main.java
+++ src/esercizio2/Main.java
@@ -7,22 +7,29 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Inserisci il numero dei chilometri percorsi");
-        int km = scanner.nextInt();
 
-        System.out.println("Inserisci la quantità di litri di carburante consumati");
-        int litri = scanner.nextInt();
 
         try {
-            int kmLitro = km / litri;
+        System.out.println("Inserisci il numero dei chilometri percorsi");
+        double km = scanner.nextDouble();
+
+        System.out.println("Inserisci la quantità di litri di carburante consumati");
+        double litri = scanner.nextDouble();
+
+        if (litri == 0) {
+            throw new ArithmeticException("Impossibile consumare 0 litri!");
+        }
+
+            double kmLitro = km / litri;
             System.out.println("La quantità di chilometri fatta per litro è: " + kmLitro);
         } catch (ArithmeticException e) {
             System.out.println("Attenzione numero non valido!");
         } catch (Exception e) {
             System.out.println("Errore: errore generale");
-            throw new RuntimeException(e);
+            scanner.nextLine();
+        } finally {
+            scanner.close();
         }
-
 
 
 
